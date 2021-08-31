65ea75227fc996bed98faf255fb3428620d9e391
ggibson571 <60713520+ggibson571@users.noreply.github.com>
2021-08-31 15:18:08 -0500

    Declared and defined functions. Added main function with set test.

--- MiniProject0/MiniProject0/ggibson_binaryutils.h
+++ MiniProject0/MiniProject0/ggibson_binaryutils.h
@@ -1,1 +1,24 @@
-#pragma once
+/*
+Author: Grace Gibson
+Date Created: 8/31/2021
+Purpose: The library of binary utility commands.
+*/
+
+//header files to include in project header file (outside header guards)
+#include <stdint.h>
+#include <stdio.h>
+
+#ifndef MINIPROJECT0 
+#define MINIPROJECT0
+
+void setbit(uint32_t* addr, uint8_t whichbit);
+
+void clearbit(uint32_t* addr, uint8_t whichbit);
+
+void setbits(uint32_t* addr, uint32_t bitmask);
+
+void clearbits(uint32_t* addr, uint32_t bitmask);
+
+void display_binary(uint32_t num);
+
+#endif
